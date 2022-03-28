e963dc66ff70e0dddbe2daf46e337446a177f8e7
JohannesDev <johannessdkortti@gmail.com>
2022-03-28 14:03:41 +0300

    app.js lisätty tavaraa yet again(const helmet yms...)

--- api/app.js
+++ api/app.js
@@ -3,11 +3,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var tilitapahtumatRouter = require('./routes/tilitapahtumat');
-var tiliRouter = require('./routes/tili');
-var userRouter = require('./routes/user');
-var korttiRouter = require('./kortti');
-var asiakasRouter = require('./routes/asiakas');
+const helmet = require('helmet');
+const cors = require('cors');
+const basicAuth = require('express-basic-auth');
+
+const tilitapahtumatRouter = require('./routes/tilitapahtumat');
+const tiliRouter = require('./routes/tili');
+const userRouter = require('./routes/user');
+const korttiRouter = require('./routes/kortti');
+const asiakasRouter = require('./routes/asiakas');
 
 var app = express();
 
@@ -17,6 +21,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(helmet());
+app.use(cors());
+app.use(basicAuth({users: { 'admin': '1234' }}))
 app.use('/tilitapahtumat', tilitapahtumatRouter);
 app.use('/asiakas', asiakasRouter);
 app.use('/kortti', korttiRouter);
