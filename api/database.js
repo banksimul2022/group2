3914829e1486dde90fe8dc35528efebd7a9e3c7b
Onni Hietanen <94108317+Hietanne@users.noreply.github.com>
2022-04-06 17:30:22 +0300

    Delete api directory

--- api/database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql');
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'pankkiautomaatti'
-});
-module.exports = connection;
--- api/models/login_model.js
+++
@@ -1,9 +0,0 @@
-const db = require('../database');
-
-const login={
-  checkPinkoodi: function(kortinnumero, callback) {
-      return db.query('SELECT Pinkoodi FROM kortti WHERE kortinnumero = ?',[kortinnumero], callback); 
-    }
-};
-          
-module.exports = login;
