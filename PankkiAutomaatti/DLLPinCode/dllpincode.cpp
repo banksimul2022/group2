eaf281ae496692e48d83910bdcebafbc63280c92
Onni Hietanen <hietanne@gmail.com>
2022-04-07 16:05:33 +0300

    testiexe tehty missä voi kokeilla DLL:ää ja joitain muutoksia koodiin

--- PankkiAutomaatti/DLLPinCode/dllpincode.cpp
+++ PankkiAutomaatti/DLLPinCode/dllpincode.cpp
@@ -2,4 +2,17 @@
 
 DLLPinCode::DLLPinCode()
 {
+    pExtraClass = new ExtraClass;
+}
+
+DLLPinCode::~DLLPinCode()
+{
+    delete pExtraClass;
+    pExtraClass = nullptr;
+}
+
+int DLLPinCode::getTries()
+{
+    var = ExtraClass().pwdTries();
+    return var;
 }
