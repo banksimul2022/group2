9cc6572416f8010a28c6eb32b2fdb3ab80eb374e
JohannesDev <johannessdkortti@gmail.com>
2022-04-21 13:42:39 +0300

    exea jatkettu lisaamalla dllpincode ja hieman restapia

--- PankkiAutomaatti/exe/debug/moc_kirjaudusisaan.cpp
+++ PankkiAutomaatti/exe/debug/moc_kirjaudusisaan.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_kirjaudusisaan_t {
-    QByteArrayData data[4];
-    char stringdata0[65];
+    QByteArrayData data[5];
+    char stringdata0[60];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -33,13 +33,14 @@ struct qt_meta_stringdata_kirjaudusisaan_t {
 static const qt_meta_stringdata_kirjaudusisaan_t qt_meta_stringdata_kirjaudusisaan = {
     {
 QT_MOC_LITERAL(0, 0, 14), // "kirjaudusisaan"
-QT_MOC_LITERAL(1, 15, 28), // "on_palaaalkuikkunaan_clicked"
-QT_MOC_LITERAL(2, 44, 0), // ""
-QT_MOC_LITERAL(3, 45, 19) // "on_kirjaudu_clicked"
+QT_MOC_LITERAL(1, 15, 24), // "on_VALIAIKANAPPI_clicked"
+QT_MOC_LITERAL(2, 40, 0), // ""
+QT_MOC_LITERAL(3, 41, 9), // "trueFalse"
+QT_MOC_LITERAL(4, 51, 8) // "pinkoodi"
 
     },
-    "kirjaudusisaan\0on_palaaalkuikkunaan_clicked\0"
-    "\0on_kirjaudu_clicked"
+    "kirjaudusisaan\0on_VALIAIKANAPPI_clicked\0"
+    "\0trueFalse\0pinkoodi"
 };
 #undef QT_MOC_LITERAL
 
@@ -49,7 +50,7 @@ static const uint qt_meta_data_kirjaudusisaan[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -57,12 +58,14 @@ static const uint qt_meta_data_kirjaudusisaan[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   24,    2, 0x08 /* Private */,
-       3,    0,   25,    2, 0x08 /* Private */,
+       1,    0,   29,    2, 0x08 /* Private */,
+       3,    1,   30,    2, 0x08 /* Private */,
+       4,    1,   33,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
-    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void, QMetaType::QString,    2,
 
        0        // eod
 };
@@ -73,12 +76,12 @@ void kirjaudusisaan::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _
         auto *_t = static_cast<kirjaudusisaan *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->on_palaaalkuikkunaan_clicked(); break;
-        case 1: _t->on_kirjaudu_clicked(); break;
+        case 0: _t->on_VALIAIKANAPPI_clicked(); break;
+        case 1: _t->trueFalse((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 2: _t->pinkoodi((*reinterpret_cast< QString(*)>(_a[1]))); break;
         default: ;
         }
     }
-    Q_UNUSED(_a);
 }
 
 QT_INIT_METAOBJECT const QMetaObject kirjaudusisaan::staticMetaObject = { {
@@ -110,13 +113,13 @@ int kirjaudusisaan::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 3)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 2;
+        _id -= 3;
     }
     return _id;
 }
