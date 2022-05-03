fca400aed70f2d77725054e723dcfd0b5da56bc1
EriKorh <t1koer00@students.oamk.fi>
2022-05-03 19:47:52 +0300

    Lisätty ehkä toimiva kortin lukitus

---
+++ api/routes/lukitus.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const lukitus = require('../models/lukitus_model');
+
+router.get('/:Kortinnumero?',
+ function(request, response) {
+  if (request.params.Kortinnumer) {
+    lukitus.getPin(request.params.Kortinnumero, function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    });
+  } else {
+    lukitus.getAll(function(err, dbResult) {
+      if (err) {
+        response.json(err);
+      } else {
+        response.json(dbResult);
+      }
+    });
+  }
+});
+
+
+router.post('/', 
+function(request, response) {
+  lukitus.add(request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(request.body);
+    }
+  });
+});
+
+
+router.delete('/:id', 
+function(request, response) {
+  lukitus.delete(request.params.id, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+
+router.put('/:Kortinnumero', 
+function(request, response) {
+  lukitus.update(request.params.Kortinnumero, request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
+
+module.exports = router;
